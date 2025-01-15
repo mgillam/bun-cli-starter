25a0911216f9a942dd630aaee6ab16cb7bae3c2f
mgillam <mic@secureideas.com>
2025-01-15 08:53:33 -0500

    Added inquirer prompt example, updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bun-cli-starter
 Starter template for Typescript CLI tools targeting Bun.
+
+For use with `degit` or `tiged`.
+
+Example:
+```bash
+npx tiged mgillam/bun-cli-starter mynewclitoolname
+```
--- src/commands/hello/world.ts
+++ src/commands/hello/world.ts
@@ -1,10 +1,18 @@
 import { Command } from "commander";
+import inquirer from "inquirer";
 
 const worldCommand = new Command("world")
     .description("Greet the world")
     .argument("[name]", "name to greet")
-    .action((name) => {
-      console.log(`Hello ${name ?? "World"}`);
+    .action(async (name) => {
+      let greetName = name;
+      if(!greetName) {
+        const { inputName } = await inquirer.prompt([ 
+         { type: "input", name: "inputName", message: "Please provide a name:", default: "World" }
+        ]);
+        greetName = inputName;
+      }
+      console.log(`Hello ${greetName}`);
 });
 
 export default worldCommand;
